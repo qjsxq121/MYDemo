5eca20108b204736f2b5238f9204f3c61f4b4800
lq <18363827786@163.com>
2018-06-08 10:40:24 +0800

    创建view2

---
+++ GitText/GitText/ViewController2222.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2222.h
+//  GitText
+//
+//  Created by lq on 2018/6/8.
+//  Copyright © 2018年 YiYoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2222 : UIViewController
+
+@end
---
+++ GitText/GitText/ViewController2222.m
@@ -0,0 +1,39 @@
+//
+//  ViewController2222.m
+//  GitText
+//
+//  Created by lq on 2018/6/8.
+//  Copyright © 2018年 YiYoff. All rights reserved.
+//
+
+#import "ViewController2222.h"
+
+@interface ViewController2222 ()
+
+@end
+
+@implementation ViewController2222
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIView *view2 = [UIView new];
+    view2.backgroundColor = [UIColor redColor];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
